8e70404cb2c6ecfda1e97c1de56a87663703f1c9
GibGummiGerd <GibGummiGerd@users.noreply.github.com>
2022-07-27 23:53:15 +0200

    logs and autonaming

--- crawler/bgg.py
+++ crawler/bgg.py
@@ -1,20 +1,39 @@
 """bgg.py"""
 
 from datetime import datetime
+from pathlib import Path
 import time
 import os
 import json
 import requests
 import pandas
+from bs4 import BeautifulSoup
 
 
 def call_stuff(game_id: str, page_id: int) -> dict:
-    # page_id = "200"
+
     url = f"https://api.geekdo.com/api/collections?objectid={game_id}&objecttype=thing&oneperuser=1&pageid={page_id}&require_review=true&showcount=50&sort=review_tstamp"
     get_response = requests.get(url)
     received_json_bytes = json.loads(get_response.content)
-    print(received_json_bytes)
+
     return received_json_bytes
+
+
+def get_game_name(game_id: str) -> str:
+    """get name of game belonging to game ID
+
+    Args:
+        game_id (int): _description_
+
+    Returns:
+        str: _description_
+    """
+    url = f"https://boardgamegeek.com/boardgame/{game_id}"
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    ld_json_tag = soup.find("script", attrs={"type": "application/ld+json"})
+    info = json.loads(ld_json_tag.string)
+
+    return info["name"]
 
 
 def create_data_structure(user_item_bytes: bytes) -> list:
@@ -56,17 +75,26 @@ def create_data_structure(user_item_bytes: bytes) -> list:
     return user_items
 
 
-def flow():
-    game_id = "292974"
+def flow(game_id: str):
+
+    game_name = get_game_name(game_id).replace(" ", "_")
 
     i = 1
-    output_path = "tournament_at_avalon_rating_items.csv"
+    output_path = os.path.join("csv", f"{game_id}_{game_name}_rating_items.csv")
+    Path("csv").mkdir(parents=True, exist_ok=True)
+
+    if os.path.exists(output_path):
+        print(
+            "File for game already exists. \nStopping processing to not overwrite content"
+        )
+        return
+
     found_items = True
     start_time = datetime.now()
 
     while found_items:
 
-        time.sleep(5)
+        time.sleep(4)
 
         json_bytes = call_stuff(game_id, i)
 
@@ -76,23 +104,29 @@ def flow():
             output_path, mode="a", header=not os.path.exists(output_path), index=False
         )
 
+        print(
+            ". . . . . . . . . . .\n"
+            + "Current run time: "
+            + str(datetime.now() - start_time)
+            + "\nRatings: "
+            + str((i - 1) * 50)
+            + " to "
+            + str(i * 50)
+        )
+
         if len(user_dict_list) == 0:
             found_items = False
-            print("xDDDDDDD")
+            print("Reached last ratings")
+            print("-----------------------------------")
 
         i = i + 1
 
-        print("Aktuelle Zeit: " + str(datetime.now() - start_time) + "\n ")
-
-    # print(cont)
-
-    # pandas.DataFrame(user_dict_list).to_csv(
-    #     "out.csv", mode="a", header=False, index=False
-    # )
-
 
 def main():
-    flow()
+
+    game_id = "167355"
+
+    flow(game_id)
 
 
 if __name__ == "__main__":
